4005d1ce189101a1b716bb8008350c4acd45751f
aqilusman45 <aqilusman45@gmail.com>
2020-04-04 04:56:44 +0500

    fix: expose port

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.get("/", (req, res) => {
   res.send("server is up and running");
 });
 
-app.listen(8000, () => {
+app.listen(8080, () => {
   console.log(`app is running on port ${8000}`);
 });
