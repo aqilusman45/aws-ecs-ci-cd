5a7c4ca0a26f60071eee1f18f1eed2bfc3f79a67
aqilusman45 <aqilusman45@gmail.com>
2020-04-04 22:59:19 +0500

    update: add new repo

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+mongoose.connect(`${process.env.DB_URI}`, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const con = mongoose.connection;
+
+con.once("open", (error) => {
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+  console.log("We are connected");
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("server is up and running");
 });
 
 app.listen(PORT, () => {
-  console.log(`app is running on port ${PORT}`);
+  console.log(`app is running on port hahah ${PORT}`);
 });
